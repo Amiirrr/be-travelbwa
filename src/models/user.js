7bbcba14ea8ee3b5a486e17511482732493d03f0
amiirrr <amirkholiluddinismail22@gmail.com>
2024-03-11 17:03:58 +0700

    [ADD] Lib-bcrypt & mongoose seed

---
+++ src/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+
+const UserSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+UserSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User;
