ee38cc3184b08768d10ab75099746fc7a9de405a
amiirrr <amirkholiluddinismail22@gmail.com>
2024-03-17 13:29:59 +0700

    [ADD] ALERT AUTH

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(router);
 // Middleware untuk CORS
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'http://localhost:3000']
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://be-travelbwa.vercel.app/']
 }))
 
 // Penanganan rute untuk rute '/'
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,11 @@
 const isLogin = (req, res, next) => {
     if (req.session.user == null || req.session.user == undefined) {
+        console.log("!isLogin")
         req.flash('alertMessage', 'Session telah habis silahkan signin kembali!!');
         req.flash('alertStatus', 'danger');
         res.redirect('/admin/signin');
     } else {
+        console.log("isLogin")
         next();
     }
 }
