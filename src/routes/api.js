470dd4c8fddabd3d2ad13dac26e697d390d797dd
amiirrr <amirkholiluddinismail22@gmail.com>
2024-03-18 12:50:30 +0700

    [DEBBUG] CORS

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,6 +6,7 @@ const api = express.Router();
 
 api.get('/landing-page', apiController.landingPage);
 api.get('/detail-page/:id', apiController.detailPage);
-api.post('/booking-page', apiController.bookingPage);
+api.get('/booking-page', apiController.bookingPage);
+api.post('/booking-page', upload, apiController.bookingPage);
 
 export default api
