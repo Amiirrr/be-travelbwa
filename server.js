5ddfe5e4590233dca6c6396942dbfc051ce625c1
amiirrr <amirkholiluddinismail22@gmail.com>
2024-03-18 13:43:54 +0700

    CORS 30001

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(router);
 // Middleware untuk CORS
 // app.use(cors())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true,
 }))
 
